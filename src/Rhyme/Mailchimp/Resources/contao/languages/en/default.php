1215b93b8f6b359b8a483054f7611455940027e9
rhymadam <adam@rhymedigital.com>
2019-07-10 16:31:09 -0400

    Ready to test

--- src/Rhyme/Mailchimp/Resources/contao/languages/en/default.php
+++ src/Rhyme/Mailchimp/Resources/contao/languages/en/default.php
@@ -15,19 +15,24 @@
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_recipients']                   = array('Recipients', 'Here you can enter a list of comma separated emails.');
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_type']                         = array('Email type', 'Send either an HTML email or a text email.');
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_date']                              = array('Delivery date', 'Set the delivery date.');
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_confirm_unschedule']                = array('Please check this box and submit to unschedule this email', '');
 
 
 /**
  * Misc
  */
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_headline']                     = 'Send a Test Email';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_needs_pause']                  = 'Please pause this campaign before sending a test.';
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_send']                         = 'Send';
-$GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_sent']                         = 'Mailchimp campaign test sent successfully';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_test_sent']                         = 'Test sent';
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_schedule_headline']                 = 'Schedule Your Campaign';
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_schedule_submit']                   = 'Schedule';
-$GLOBALS['TL_LANG']['MSC']['mailchimp_email_scheduled']                         = 'Mailchimp campaign scheduled successfully';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_scheduled']                         = 'Campaign scheduled';
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_scheduled_for']                     = 'This campaign has been scheduled for %s';
 $GLOBALS['TL_LANG']['MSC']['mailchimp_email_unschedule_headline']               = 'Unschedule Your Campaign';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_unschedule_submit']                 = 'Unschedule';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_unschedule_confirm']                = 'Please check this box and submit to unschedule this email';
+$GLOBALS['TL_LANG']['MSC']['mailchimp_email_unscheduled']                       = 'This campaign has been unscheduled';
 
 
 /**
